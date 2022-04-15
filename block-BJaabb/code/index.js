a2d56c1c34b73c4ad06c4a40689a1a9d069e99fa
Rajnish <mr.rajnish2000@gmail.com>
2022-04-15 21:19:47 +0530

    block-BJaabb

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,22 +5,48 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
+let user = prompt ("Enter a username");
+    alert(user);
+  let userName = prompt("Enter a username");
+      console.log(user,userName);
+  let age = 22; let isLearning = true;
+   user = "Jhon" ;
+   console.log(user, userName) ;             
 
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
-
+ let numA = prompt("Enter a numA");
+ let numB = prompt("Enter a numB");
+ let sum = numA + numB ;
+  alert(sum);
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++){
+     if(j % 2 === 0){
+       } console.log(j);
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+  for( let i = 1; i<9 ; i--){
+      console.log(i);
+  }
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let intialValue = 0;
+ while(intialValue <= 5){
+     console.log(intialValue);
+     intialValue--;
+ }
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+let evenValue = 0;
+ while(intialValue <= 10){
+     console.log(intialValue);
+     intialValue--;
+ }
 
 // Comparisoin
 
@@ -36,3 +62,7 @@ Example:
 "hello", -21; // false
 
 */
+let num1 = Number(prompt("Enter your num1"));
+let num2 = Number(prompt("Enter your num2"));
+   let numAdded = num1 === num2;
+      alert(numAdded,"Both number are equal");
